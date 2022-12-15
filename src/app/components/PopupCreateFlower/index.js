5a31fd4cf3a915b34e572fdf2a30ad2b95429ba1
whoant <vovanhoangtuan4.2@gmail.com>
2022-12-15 17:09:37 +0700

    Add

--- src/app/components/PopupCreateFlower/index.js
+++ src/app/components/PopupCreateFlower/index.js
@@ -1,157 +1,212 @@
-import { Button, Form, Input, Modal, Select } from 'antd';
+import { Button, Space, Form, Input, Modal, Select } from 'antd';
 import React, { useEffect, useState } from 'react';
-import { auto } from '@cloudinary/url-gen/qualifiers/quality';
 import categoryApi from "../../api/category";
 import colorApi from "../../api/color";
+import budApi from "../../api/bud";
+import flowerApi from "../../api/flower";
+import { toastSuccess } from '../Toast';
+
 
 const PopupCreateFlower = (props) => {
-    const [form] = Form.useForm();
-    const [images, setImage] = useState([]);
-    const [colors, setColors] = useState([]);
-    const [categories, setCategories] = useState([]);
-    const { isModalOpen, handleOk, handleCancel } = props
+        const [form] = Form.useForm();
+        const [images, setImage] = useState([]);
+        const [colors, setColors] = useState([]);
+        const [categories, setCategories] = useState([]);
+        const [buds, setBuds] = useState([]);
+        const [detailBuds, setDetailBuds] = useState([]);
+        
+        const { isModalOpen, handleCancel, handleOk } = props
 
-    useEffect(() => {
-        const listCategories = async () => {
-            try {
+        useEffect(() => {
+            const listCategories = async() => {
                 const { data } = await categoryApi.getAllCategories();
                 setCategories(data.categories)
-            } catch (e) {
-                console.error(e)
-            }
-        };
+            };
 
-        listCategories();
-    }, []);
+            listCategories();
+        });
 
-    useEffect(() => {
-        const listColors = async () => {
-            try {
+        useEffect(() => {
+            const listColors = async() => {
                 const { data } = await colorApi.getAllColors();
                 setColors(data.colors)
-            } catch (e) {
-                console.error(e)
-            }
+            };
+
+            listColors();
+        }, []);
+
+        useEffect(() => {
+            const listBuds = async() => {
+                const { data } = await budApi.getAllBuds();
+                setBuds(data.buds)
+            };
+
+            listBuds();
+        }, []);
+
+        const handleUpload = () => {
+            setImage([])
+            const myWidget = window.cloudinary.createUploadWidget({
+                    cloudName: 'dhhryzno4',
+                    uploadPreset: 'faeuzoyi',
+                    folder: "sell_flower",
+                    multiple: true,
+                }, (error, result) => {
+                    if (!error && result && result.event === "success") {
+                        setImage((pre =>
+                                [...pre, result.info.url]
+                        ));
+                    }
+                }
+            )
+            myWidget.open()
         };
 
-        listColors();
-    }, []);
+        const onFinishForm = async(e) => {
+            const value = { ...e, images, bud_data: detailBuds };
+            await flowerApi.createFlower(value);
+            toastSuccess('Thêm dữ liệu thành công');
+            handleOk();
+            setDetailBuds([]);
+            form.resetFields();
+        };
 
-    const handleUpload = () => {
-        setImage([])
-        const myWidget = window.cloudinary.createUploadWidget({
-                cloudName: 'dhhryzno4',
-                uploadPreset: 'faeuzoyi',
-                folder: "sell_flower",
-                multiple: true,
-            }, (error, result) => {
-                if (!error && result && result.event === "success") {
-                    setImage((pre =>
-                            [...pre, result.info.url]
-                    ));
+        const handleChangeBud = (e) => {
+            const data = e.map(id => {
+                const currentBud = buds.find(bud => bud.id === id);
+                return {
+                    ...currentBud,
+                    count: 1
                 }
-            }
-        )
-        myWidget.open()
-    };
+            });
 
-    const onFinishForm = (e) => {
-        const value = { ...e, images }
-        console.log(value)
-        handleOk(value)
-        form.resetFields();
-    }
+            setDetailBuds(data);
+        };
 
-    return (
-        <>
-            <Modal title="Tạo sản phẩm mới" open={isModalOpen}
-                   onCancel={handleCancel} footer={null}>
-                <Form labelCol={{ span: 6 }}
-                      wrapperCol={{ span: 18 }}
-                      form={form}
-                      onFinish={onFinishForm}
-                >
-                    <Form.Item
-                        label="Tên hoa"
-                        name="name"
+        const handleChangeCount = (event, bud) => {
+            const newDetailBud = [...detailBuds];
+            const index = newDetailBud.findIndex(detailBud => detailBud.id === bud.id);
+            newDetailBud[index].count = Number(event.target.value);
+            setDetailBuds(newDetailBud);
+        };
+
+        return (
+            <>
+                <Modal title="Tạo sản phẩm mới" open={isModalOpen}
+                       onCancel={handleCancel} footer={null}>
+                    <Form labelCol={{ span: 6 }}
+                          wrapperCol={{ span: 18 }}
+                          form={form}
+                          onFinish={onFinishForm}
                     >
-                        <Input/>
-                    </Form.Item>
+                        <Form.Item
+                            label="Tên hoa"
+                            name="name"
+                        >
+                            <Input/>
+                        </Form.Item>
 
-                    <Form.Item
-                        label="Loại hoa"
-                        name="category">
-                        <Select>
+                        <Form.Item
+                            label="Loại hoa"
+                            name="category_ids">
+                            <Select mode="multiple">
+                                {
+                                    (categories.length > 0 ? (categories.map(category => {
+                                        return <Select.Option
+                                            value={category.id}>{category.title}
+                                        </Select.Option>
+                                    })) : <></>)
+                                }
+                            </Select>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Màu"
+                            name="color">
+                            <Select>
+                                {
+                                    (colors.length > 0 ? (colors.map(color => {
+                                        return <Select.Option
+                                            value={color}>{color}
+                                        </Select.Option>
+                                    })) : <></>)
+                                }
+                            </Select>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Giá gốc"
+                            name="original_price">
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item
+                            label="Giá giảm"
+                            name="sale_price">
+                            <Input/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Mô tả"
+                            name="description">
+                            <Input/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Hình"
+                            name="images">
                             {
-                                (categories.length > 0 ? (categories.map(category => {
-                                    return <Select.Option
-                                        value={category.id}>{category.title}
-                                    </Select.Option>
-                                })) : <></>)
-                            }
-                        </Select>
-                    </Form.Item>
-
-                    <Form.Item
-                        label="Màu"
-                        name="color">
-                        <Select>
-                            {
-                                (colors.length > 0 ? (colors.map(color => {
-                                    return <Select.Option
-                                        value={color}>{color}
-                                    </Select.Option>
-                                })) : <></>)
-                            }
-                        </Select>
-                    </Form.Item>
-
-                    <Form.Item
-                        label="Giá gốc"
-                        name="original_price">
-                        <Input/>
-                    </Form.Item>
-
-                    <Form.Item
-                        label="Mô tả"
-                        name="description">
-                        <Input/>
-                    </Form.Item>
-
-                    <Form.Item
-                        label="Hình"
-                        name="images"
-                    >
-                        {
-                            images.length == 0 ?
-                                <Button onClick={handleUpload}>Upload</Button> :
-                                <div>
-                                    <div className='modal_create-product'>
-                                        {
-                                            images.map(e => {
-                                                return <img src={e} style={{
-                                                    width: 100,
-                                                    height: auto
-                                                }}/>
-                                            })
-                                        }
+                                images.length === 0 ?
+                                    <Button onClick={handleUpload}>Upload</Button> :
+                                    <div>
+                                        <div className='modal_create-product'>
+                                            {
+                                                images.map(e => {
+                                                    return <img src={e} style={{
+                                                        width: 100,
+                                                        height: 100
+                                                    }} alt=''/>
+                                                })
+                                            }
+                                        </div>
+                                        <Button onClick={() => {
+                                            setImage([])
+                                        }}>
+                                            Hủy
+                                        </Button>
                                     </div>
-                                    <Button onClick={() => {
-                                        setImage([])
-                                    }}>
-                                        Hủy
-                                    </Button>
-                                </div>
 
+                            }
+                        </Form.Item>
+                        <Form.Item
+                            label="Búp hoa"
+                            name="bud">
+                            <Select mode="multiple" onChange={handleChangeBud}>
+                                {
+                                    (buds.length > 0 ? (buds.map(bud => {
+                                        return <Select.Option
+                                            value={bud.id}>{bud.name}
+                                        </Select.Option>
+                                    })) : <></>)
+                                }
+                            </Select>
+                        </Form.Item>
+                        {
+                            (detailBuds.length > 0 ? (detailBuds.map(detailBud => {
+                                return <Space.Compact key={detailBud.id} block style={{ 'margin-bottom': '24px' }}>
+                                    <Input defaultValue={detailBud.name} disabled/>
+                                    <Input defaultValue={detailBud.count} onChange={e => handleChangeCount(e, detailBud)}/>
+                                </Space.Compact>
+                            })) : <></>)
                         }
-                    </Form.Item>
-                    <div className='modal_btn'>
-                        <Button htmlType='submit'>Ok</Button>
-                        <Button onClick={handleCancel}>Cancel</Button>
-                    </div>
-                </Form>
-            </Modal>
-        </>
-    );
-};
+
+                        <div className='modal_btn'>
+                            <Button htmlType='submit' type="primary">Ok</Button>
+                            <Button onClick={handleCancel}>Cancel</Button>
+                        </div>
+                    </Form>
+                </Modal>
+            </>
+        );
+    }
+;
 export default PopupCreateFlower;
